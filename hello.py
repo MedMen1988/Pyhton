bd16f4192f16020f4ffff6794d9e4e1546e24b94
Mr.MedMen <laa10081988@gmail.com>
2020-07-26 18:48:25 +0300

    3 задание

--- hello.py
+++ hello.py
@@ -9,17 +9,13 @@ a = int(input("Введите время в секундах "))
 h = a//3600
 m = (a//60)%60
 s = a%60
-if h<10:
-    h=str('0'+str(h))
-else:
-    h=str(h)
-if m<10:
-    m=str('0'+str(m))
-else:
-    m=str(m)
-if s<10:
-    s=str('0'+str(s))
-else:
-    s=str(s)
-print(f"Ваше время {str(h)}:{str(m)}:{str(s)}")
+print( "Ваше время "'%02d:%02d:%02d'% (h, m, s) )
 
+print("\nЗадание 3")
+a = int(input("Найдите сумму чисел n + nn + nnn. Введите число "))
+b = str(a)
+b1 = b + b
+b2 = b + b + b
+c = a + int(b1) + int(b2)
+
+print(f"Результат {c}")
